f861349c50a6da0be97299d53e55740e4b5246c2
ooooevan <635638508@qq.com>
2021-01-17 22:03:51 +0800

    chore: 增加commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,14 @@
+/**
+feat: 新特性
+fix: 修复bug
+docs: 修改文档
+style: 修改代码样式（如空格、分号、格式化等）
+refactor: 重构代码（不增减功能）
+perf: 增加性能
+test: 增加测试
+chore: 构建过程或辅助工具修改
+
+其他：build、ci、revert
+*/
+
+module.exports = { extends: ['@commitlint/config-conventional'] }
